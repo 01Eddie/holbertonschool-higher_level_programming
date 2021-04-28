f8a20c8df00190e26f104766dc0cc403ab88abf2
01Eddie <jesus.11.97@hotmail.com>
2021-04-28 10:38:19 -0500

    [add] 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') -1, -1):
+for i in range(ord('z'), ord('a')-1, -1):
     may = chr(i)
-    if i%2 != 0:
+    if i % 2 != 0:
         may = chr(i - 32)
-    print("{}".format(may), end = '')
+    print("{}".format(may), end='')
