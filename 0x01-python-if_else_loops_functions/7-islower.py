f33f89be4a2efb7f01a3df64082e4923b626cac3
01Eddie <jesus.11.97@hotmail.com>
2021-04-27 23:48:12 -0500

    [ADD] 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def islower(c):
-    for i in range(ord('a'), ord('z'), 1):
+    for i in range(ord('a'), ord('z') + 1):
         if ord(c) == i:
             return True
     return False
