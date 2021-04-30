4cd6c54e6b85e46a409ac2c9e41192df1912c371
01Eddie <jesus.11.97@hotmail.com>
2021-04-29 20:30:48 -0500

    [ADD] 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,9 +5,5 @@ if __name__ == "__main__":
     s = len(sys.argv) - 1
     for i in range(1, s + 1, 1):
         var = sys.argv[i]
-        if not str(var).isdigit():
-            print("No number in args")
-            break
-        else:
-            su += int(var)
+        su += int(var)
     print("{:d}".format(su))
