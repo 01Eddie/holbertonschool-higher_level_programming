01339944312e27c9b944158448cb0f2727653f17
01Eddie <jesus.11.97@hotmail.com>
2021-05-05 00:29:03 -0500

    [ADD] 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     size = len(sentence)
-    if size == 0:
+    if size == 0 or size < 0:
         sentence[0] = "None"
     else:
         return size, sentence[0]
