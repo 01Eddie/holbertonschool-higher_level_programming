4be599888cbccbca018e181ef7ddf7553df6c3c9
01Eddie <jesus.11.97@hotmail.com>
2021-05-04 22:43:29 -0500

    [ADD] 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0:
         print()
     else:
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i])):
                 if j < len(matrix[i]) - 1:
                     print("{:d}".format(matrix[i][j]), end=' ')
                 else:
