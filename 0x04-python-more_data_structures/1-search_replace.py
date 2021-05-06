031221d92e06a94364446bbfa12e4bf79751af92
01Eddie <jesus.11.97@hotmail.com>
2021-05-06 14:40:25 -0500

    [ADD] 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     for i in range(0, len(my_list), 1):
         if my_list[i] == search:
             newL[j] = replace
-        j+=1
+        j += 1
     return newL
