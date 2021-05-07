0053f84104f01b6953b78ae362bb9b5a87109859
01Eddie <jesus.11.97@hotmail.com>
2021-05-07 11:15:42 -0500

    [ADD] 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if (
-        roman_string is None
-        or not roman_string
-        or not isinstance(roman_string, str)
+        roman_string is None or
+        not roman_string or
+        not isinstance(roman_string, str)
     ):
         return 0
     val = {
