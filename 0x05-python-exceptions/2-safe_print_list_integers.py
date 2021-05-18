45b9b16e96f93df446d38237826d7e6bfe5ec122
01Eddie <jesus.11.97@hotmail.com>
2021-05-18 03:01:22 -0500

    [ADD] 5-raise_exception.py 4-list_division.py 3-safe_print_division.py 2-safe_print_list_integers.py 1-safe_print_integer.py 6-raise_exception_msg.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):    
+def safe_print_list_integers(my_list=[], x=0):
     # declarar e inicializar el contador
     counter = 0
     # declara variable auxiliar para imprimir la linea de elementos
     line = ""
     # iterar los elementos del array con la variable i en el rango de x
     for i in range(x):
-        # si el contador < x
-        if counter < x: # 0 < 2 x -- lo compara al valor introducido en la funcion
+
+        # si el contador<x
+        if counter < x:
             try:
-                # imprimir el elemento i del array my_list
                 print("{:d}".format(my_list[i]), end="")
-                # aumentamos counter en 1
                 counter += 1
-                # ValueError: Se genera cuando una operación o función recibe un argumento que tiene el tipo correcto pero un valor inapropiado
-                # TypeError: Se genera cuando se aplica una operación o función a un objeto de tipo inadecuado. El valor asociado es una cadena que proporciona detalles sobre la falta de coincidencia de tipos.
             except (ValueError, TypeError):
                 continue
     print()
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def raise_exception():
-    #raise: La declaración de aumento permite al programador forzar la ocurrencia de una excepción específica
+    # raise: La declaración de aumento permite al
+    # programador forzar la ocurrencia de una excepción específica
     raise TypeError
