b8f5932195deeaca663aaa46ef25205c4decbee7
01Eddie <jesus.11.97@hotmail.com>
2021-05-18 23:32:52 -0500

    [ADD] 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define or write class Square """
+
+
+class Square:
+    """Define the variable or attribute in the principal method"""
+    def __init__(self, size):
+        """The __ define the attribute in private instance"""
+        self.__size = size
