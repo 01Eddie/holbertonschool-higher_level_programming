1064ab7bc5a133b7ea93643f48bdc444feee9b5b
01Eddie <jesus.11.97@hotmail.com>
2021-05-18 23:51:54 -0500

    [ADD] 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" define or write class Square"""
 
 
 class Square:
+    """Define the variable or attribute in the principal method"""
     def __init__(self, size=0, position=(0, 0)):
+        """The __ define the attribute in private instance"""
         self.__size = size
         self.__position = position
 
