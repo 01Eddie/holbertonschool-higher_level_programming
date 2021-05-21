a7f3538e806635663e2ca872bd0dc9132d3b5f88
01Eddie <jesus.11.97@hotmail.com>
2021-05-21 16:06:41 -0500

    [ADD] files mandatory

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 
-def text_indentation(text):
-    if type(text) is not str :
-        raise TypeError("text must be a string")
-    print("{}".format(text))
+
+def print_square(size):
+        """if type of value if is integer"""
+        if type(size) is not int:
+            """print the error"""
+            raise TypeError("size must be an integer")
+        """if size is number negative"""
+        if size < 0:
+            """print the error"""
+            raise ValueError("size must be >= 0")
+        if size == 0 or size is None:
+            print()
+        else:
+            for i in range(0, size):
+                print("{}".format('#' * size))
