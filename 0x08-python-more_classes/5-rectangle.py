97808cb4d2fbf9000c1ceb6fd41072da33c6a165
01Eddie <jesus.11.97@hotmail.com>
2021-05-25 00:48:02 -0500

    [ADD] 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,44 +1,32 @@
 #!/usr/bin/python3
+"""
+defines class Rectangle
+"""
 
-"""# Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)
-# 
-# Private instance attribute: width:
-# property def width(self): to retrieve it
-# property setter def width(self, value): to set it:
-# width must be an integer, otherwise raise a TypeError exception with the message width must be an integer
-# if width is less than 0, raise a ValueError exception with the message width must be >= 0
-# Private instance attribute: height:
-# property def height(self): to retrieve it
-# property setter def height(self, value): to set it:
-# height must be an integer, otherwise raise a TypeError exception with the message height must be an integer
-# if height is less than 0, raise a ValueError exception with the message height must be >= 0
-# Instantiation with optional width and height: def __init__(self, width=0, height=0):
-# Public instance method: def area(self): that returns the rectangle area
-# Public instance method: def perimeter(self): that returns the rectangle perimeter:
-# if width or height is equal to 0, perimeter has to be equal to 0
-# print() and str() should print the rectangle with the character #:
-# if width or height is equal to 0, return an empty string
-# repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval()
-# Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted
-# You are not allowed to import any module"""
 
 class Rectangle:
+    """Define the variable or attribute in the principal method"""
     def __init__(self, width=0, height=0):
-        self.__width = width
+        """The __ define the attribute in private instance"""
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
+        """property getter method"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if value == None:
+        """property setter method"""
+        if value is None:
             raise TypeError("width must be an integer")
-        """# width must be an integer, otherwise raise a TypeError exception with the message width must be an integer"""
+
+        """TypeError exception with the message width must be an integer"""
         if (type(value) is not int and type(value) is not float):
             raise TypeError("width must be an integer")
-        """# if width is less than 0, raise a ValueError exception with the message width must be >= 0"""
+
+        """ValueError exception with the message width must be >= 0"""
         if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -46,30 +34,35 @@ class Rectangle:
 
     @property
     def height(self):
+        """property getter method"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if value == None:
+        """property setter method"""
+        if value is None:
             raise TypeError("height must be an integer")
-        """# height must be an integer, otherwise raise a TypeError exception with the message height must be an integer"""
+        """TypeError exception with the message height must be an integer"""
         if (type(value) is not int and type(value) is not float):
             raise TypeError("height must be an integer")
-        """# if height is less than 0, raise a ValueError exception with the message height must be >= 0"""
+        """ValueError exception with the message height must be >= 0"""
         if (value <= 0):
             raise ValueError("height must be >= 0")
         self.__height = value
         return value
 
-    """# Public instance method: def area(self): that returns the rectangle area"""
+    """Public instance method: def area"""
     def area(self):
+        """returns the rectangle area"""
         return self.__width * self.__height
 
-    """# Public instance method: def perimeter(self): that returns the rectangle perimeter:"""
+    """Public instance method: def perimeter"""
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
+        """returns the rectangle perimeter:"""
         return (self.__width + self.__height) * 2
+
     """# for print the #"""
     def __str__(self):
         rectangle = []
@@ -84,10 +77,11 @@ class Rectangle:
         rectangle = "".join(rectangle)
         return rectangle
 
-    """# repr() should return a string representation of the rectangle to be able to recreate a new instance by using eval() (see example below)"""
+    """repr() should return a string representation of the rectangle"""
     def __repr__(self):
+        """return a string representation of the rectangle"""
         return "Rectangle({}, {})".format(self.__height, self.__width)
 
-    """# Print the message Bye rectangle... (... being 3 dots not ellipsis) when an instance of Rectangle is deleted"""
+    """Print the message Bye rectangle..."""
     def __del__(self):
         print("Bye rectangle...")
