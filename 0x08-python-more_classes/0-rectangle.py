83979d7a9264ae401d432c23fec6d0616229b413
01Eddie <jesus.11.97@hotmail.com>
2021-05-25 00:11:14 -0500

    [ADD] 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Write an empty class Rectangle that defines a rectangle:
-# You are not allowed to import any module"""
+"""
+Write an empty class Rectangle that defines a rectangle:
+# You are not allowed to import any module
+"""
 
 
 class Rectangle:
+    """It returned pass for that's empty"""
     pass
