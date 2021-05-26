8e41accb0294ae543fcb80da7b5ba5fdcd77f5aa
01Eddie <jesus.11.97@hotmail.com>
2021-05-26 01:08:15 -0500

    [ADD] all files

--- 0x09-python-everything_is_object/int.py
+++ 0x09-python-everything_is_object/int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def __init__(self):
-    print("I")
-    print("Love")
-    print("Python")
+print(type("I"))
+print("Love")
+print("Python")
+""" 
 import dis
-dis.dis(__init__)
+dis.dis(__init__) """
