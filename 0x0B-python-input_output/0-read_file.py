052b5869fa9b7d91f7ed193272bea1cf265f3509
01Eddie <jesus.11.97@hotmail.com>
2021-06-02 14:52:02 -0500

    [ADD] 0-read_file.py and README.md

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """The with statement"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print("{}".format(line), end="")
+ 
