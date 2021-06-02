c162c1dfc354c727de502fc4ecc2a8206ded7f8d
01Eddie <jesus.11.97@hotmail.com>
2021-06-02 01:40:08 -0500

    [ADD] 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""class Square inheriting from the last exercise"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    """public method def area"""
+    def area(self):
+        return self.__size ** 2
+
+    """public method magic"""
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
