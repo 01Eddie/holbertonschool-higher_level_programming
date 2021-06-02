2d9f48910edfd84791343b50443e531ce9e54ec0
01Eddie <jesus.11.97@hotmail.com>
2021-06-01 22:45:36 -0500

    [ADD] 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry:
+    def __init__(self):
+        pass
+    """public method def area"""
+    def area(self):
+        raise Exception("area() is not implemented")
