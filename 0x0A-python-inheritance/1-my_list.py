1b5199ff428ab9d7acb8788969613aa96bf8371e
01Eddie <jesus.11.97@hotmail.com>
2021-06-01 20:03:54 -0500

    [ADD] 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""
+Class MyList that inherits from list
+    Public instance method: def print_sorted(self): that prints
+    the list, but sorted (ascending sort)
+"""
 
-# Write a class MyList that inherits from list:
 
-# Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
-# You can assume that all the elements of the list will be of type int
-# You are not allowed to import any module
 class MyList(list):
     def __init__(self):
         pass
+    """Can assume that all the elements of the list will be of type int"""
     def print_sorted(self):
         print(sorted(self))
