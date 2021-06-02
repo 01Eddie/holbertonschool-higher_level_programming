e7f06912c4242be0a97654a1dfd6863bc5ba56a1
01Eddie <jesus.11.97@hotmail.com>
2021-06-02 01:23:06 -0500

    [ADD] 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,6 +6,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ principal method """
     def __init__(self, size):
+        """ The built-in super () function allows you to
+        invoke a method of a parent class from a child class. """
         super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
