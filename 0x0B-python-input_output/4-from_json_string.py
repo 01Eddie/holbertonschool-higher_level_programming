1d43fc83b52d93c5128f98f9defbedc40529ab34
01Eddie <jesus.11.97@hotmail.com>
2021-06-02 22:23:53 -0500

    [ADD] 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that returns an object represented by a JSON string"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Converting string to json"""
+    return json.loads(my_str)
