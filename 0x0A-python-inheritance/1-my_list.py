b7323e9feecd1b60ec82531ff50b759b447fcf8d
01Eddie <jesus.11.97@hotmail.com>
2021-06-03 11:40:06 -0500

    [ADD] 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,8 +7,10 @@ Class MyList that inherits from list
 
 
 class MyList(list):
+    """Principal Method"""
     def __init__(self):
         pass
     """Can assume that all the elements of the list will be of type int"""
     def print_sorted(self):
+        """Print List ascending sort"""
         print(sorted(self))
