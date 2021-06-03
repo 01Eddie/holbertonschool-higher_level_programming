cc6523b8df0093743e3230c5b2917802c6c993b4
01Eddie <jesus.11.97@hotmail.com>
2021-06-02 20:21:27 -0500

    [ADD] 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import json
 """Function that returns the JSON representation of an object (string)"""
 
+import json
 
 def to_json_string(my_obj):
     """printing initial json"""
-    return json.dumps(my_obj)
+    initialization_string = json.dumps(my_obj)
+    return initialization_string
