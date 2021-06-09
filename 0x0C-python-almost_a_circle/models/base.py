624ea2bc7577f4874043d6b22814a67eea1ed50a
01Eddie <jesus.11.97@hotmail.com>
2021-06-08 23:20:47 -0500

    [modified] models/base.py function to_json_string(list_dictionaries)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Class Base: """
+import json
 
 
 class Base:
@@ -7,11 +8,24 @@ class Base:
     __nb_objects = 0
     """  class constructor """
     def __init__(self, id=None):
-        """ if id is not None, assign the public instance attribute id with this argument value - you can assume id is an integer and you don’t need to test the type of it """
+        """ if id is not None, assign the public
+        instance attribute id with this argument
+        value - you can assume id is an integer and
+        you don’t need to test the type of it """
         if id is not None:
-            """ pide at.. publico """
+            """pide at.. publico"""
             self.id = id
         else:
-            """ otherwise, increment __nb_objects and assign the new value to the public instance attribute id """
+            """ otherwise, increment __nb_objects
+            and assign the new value to the public
+            instance attribute id """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation
+        of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
