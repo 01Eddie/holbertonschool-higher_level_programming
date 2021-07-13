346608ae8a8d5aa8e637e8f6800ff9554a93d614
01Eddie <jesus.11.97@hotmail.com>
2021-07-13 02:16:48 -0500

    [ADD] modified the exercise 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -12,13 +12,13 @@
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
 
-CREATE TABLE IF NOT EXISTS second_table (
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO  `second_table`(`id`, `name`, `score`)
+INSERT INTO second_table( `id`, `name`, `score`)
 VALUES(1, 'Jhon', 10),
-VALUES(2, 'Alex', 3),
-VALUES(3, 'Bob', 14),
-VALUES(4, 'George', 8);
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
