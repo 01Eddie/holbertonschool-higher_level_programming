345638364dd73e34bf6e1dda1f001d838928699f
01Eddie <jesus.11.97@hotmail.com>
2021-08-03 02:16:29 -0500

    [ADD] 1-13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-let next = 0;
 
 if (argv.length > 3) {
   argv.sort();
-  next = argv[argv.length - 2];
-  console.log(next);
+  console.log(argv[argv.length - 2]);
 } else {
   console.log(0);
 }
