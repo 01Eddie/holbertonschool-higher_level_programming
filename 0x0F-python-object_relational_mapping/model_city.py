569042bea4cdf44f7629e024d632fe18043b47ed
01Eddie <jesus.11.97@hotmail.com>
2021-08-08 23:28:53 -0500

    [fixed] model_city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Python file similar to model_state.py named model_city.py that
+contains the class definition of a City"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.schema import ForeignKey
@@ -6,6 +8,7 @@ from model_state import Base, State
 
 
 class City(Base):
+    """links to the MySQL table cities"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
