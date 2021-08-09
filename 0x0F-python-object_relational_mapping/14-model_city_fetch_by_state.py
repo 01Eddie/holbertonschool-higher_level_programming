8e994f8aa5b9502bddc7f8af67784783dbd97ce6
01Eddie <jesus.11.97@hotmail.com>
2021-08-08 21:32:00 -0500

    [fixed] 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     eqCS = City.state_id == State.id
-    cities = session.query(City, State).filter(eqCS).order_by(City.id)
+    cities = session.query(City, State).filter(eqCS).order_by(City.id).all()
     for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
