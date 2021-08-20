5a106087840ffb328ee348f1b09350b86204fc36
01Eddie <jesus.11.97@hotmail.com>
2021-08-19 23:31:06 -0500

    [ADD] task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,16 @@
 
 
 def find_peak(list_of_integers):
-    list_of_integers.sort()
-    for i in range(len(list_of_integers)):
-        return list_of_integers[i-1]
+
+    sizeArr = len(list_of_integers) - 1
+    index = 0
+    middle = sizeArr
+    if sizeArr == -1:
+        return None
+    while (index < sizeArr):
+        middle = (index + sizeArr) // 2
+        if list_of_integers[middle] < list_of_integers[middle + 1]:
+            index = middle + 1
+        else:
+            sizeArr = middle
+    return list_of_integers[index]
