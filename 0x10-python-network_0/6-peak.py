10bd12c7a5d9a995206281f284f6d158239667f8
01Eddie <jesus.11.97@hotmail.com>
2021-08-19 23:36:02 -0500

    [ADD] task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+""" Find_peak: function that find peak"""
 
 
 def find_peak(list_of_integers):
-
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak
+    Returns: peak of list_of_integers[index] or None
+    """
     sizeArr = len(list_of_integers) - 1
     index = 0
     middle = sizeArr
