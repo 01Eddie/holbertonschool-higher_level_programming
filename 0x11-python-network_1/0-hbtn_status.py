9d0624cce34b4478232eb46d7d1fee97879e585f
01Eddie <jesus.11.97@hotmail.com>
2021-08-20 17:11:59 -0500

    [ADD] task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+
+
+if __name__ == "__main__":
+    """ Python script that fetches """
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}\n\t- content: {}\n\t- utf content: {}'.
+              format(type(html), html, html.decode('utf-8')))
