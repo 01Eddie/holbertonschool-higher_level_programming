eb9f6764d0a048a37099535b5de496f7763019b4
01Eddie <jesus.11.97@hotmail.com>
2021-08-20 19:14:14 -0500

    [ADD] task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,7 @@
 if __name__ == "__main__":
     """ Python script that fetches """
     import urllib.request as request
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
