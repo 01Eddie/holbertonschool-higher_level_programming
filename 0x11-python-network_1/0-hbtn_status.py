e6a24e66c09ee34db43ed1a80e83b239fd499e9c
01Eddie <jesus.11.97@hotmail.com>
2021-08-20 19:24:23 -0500

    [ADD] task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+from urllib import request
 """
 Show Status
-Python script that fetches
+Python script that fetches https://intranet.hbtn.io/status
 """
 
 
 if __name__ == "__main__":
-    from urllib import request
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
-        print('Body response:')
+        print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf content: {}".format(html.decode('utf-8')))
