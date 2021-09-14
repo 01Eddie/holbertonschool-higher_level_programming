a82d528d71d5b8eeda06f5ebf303435e9238752c
01Eddie <jesus.11.97@hotmail.com>
2021-09-14 18:33:57 -0500

    [ADD] task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const fs = require('fs/promises');
+const request = require('request');
+
+/**
+ * Write a script that gets the contents of a webpage and stores it in a file.
+ *
+ * The first argument is the URL to request
+ * The second argument the file path to store the body response
+ * The file must be UTF-8 encoded
+ * You must use the module request
+ */
+
+const arLink = argv[2];
+const arText = argv[3];
+
+request(arLink, (err, res, body) => {
+  (err) ? console.log(err) : fs.writeFile(arText, body, 'utf-8');
+});
