05540a03c9777a1bdbac7174d6e6f0c0327ea7fd
01Eddie <jesus.11.97@hotmail.com>
2021-09-14 19:59:27 -0500

    [fix] task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
+const { argv } = require('process');
+const request = require('request');
+
 /**
  * Write a script that computes the number of tasks completed by user id.
  *
@@ -7,3 +10,19 @@
  * Only print users with completed task
  * You must use the module request
  */
+
+request(argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    const taskByUsers = {};
+    body = JSON.parse(body);
+    for (let i = 0; i < body.length; i++) {
+      const IDuser = body[i].userId;
+      const complete = body[i].completed;
+
+      if (complete && !taskByUsers[IDuser]) taskByUsers[IDuser] = 0;
+      if (complete) taskByUsers[IDuser]++;
+    }
+    console.log(taskByUsers);
+  }
+});
